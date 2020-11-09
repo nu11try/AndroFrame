7f7348b75d0eea0599c803a4f1a3f9c248d73b50
nu11try <45148694+nu11try@users.noreply.github.com>
2020-11-09 16:54:52 +0300

    Update BasePage.py

--- module/core/basepage/BasePage.py
+++ module/core/basepage/BasePage.py
@@ -8,6 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
 
     def __init__(self, driver):
+        """
+        Конструктор
+        :param driver: WebDriver
+        """
         self.driver = driver
 
     # ----------------------- ПОИСК ЭЛЕМЕНТОВ -----------------------
@@ -38,23 +42,50 @@ class BasePage:
 
     # ----------------------- ДЕЙСТВИЯ НАД ЭЛЕМЕНТАМИ -----------------------
     def click_element(self, element):
+        """
+        Клик по элементу
+        :param element: элемент
+        :return: None
+        """
         element.click()
 
     def clear_input(self, locator):
+        """
+        Очистить поле ввода
+        :param locator: путь к элементу
+        :return: None
+        """
         element = self.find_element(locator)
         len_input_text = len(element.get_attribute("value"))
         for index in range(len_input_text):
             element.send_keys("\b")
 
     def write_in_element(self, locator, words):
+        """
+        Заполнение поля ввода
+        :param locator: путь к элементу
+        :param words: строка
+        :return: None
+        """
         element = self.find_element(locator)
         element.send_keys(words)
 
     # ----------------------- ОЖИДАНИЯ -----------------------
     def wait(self, sec):
+        """
+        Ожидание через засыпания главного потока
+        :param sec: секунды
+        :return: None
+        """
         time.sleep(sec)
 
     def wait_view_element(self, locator, time):
+        """
+        Ожидание появления элемента
+        :param locator: путь к элементу
+        :param time: время ожидания
+        :return: False - элемент не появился True - элемент появился
+        """
         element = False
         try:
             element = WebDriverWait(self.driver, time).until(
@@ -65,6 +96,11 @@ class BasePage:
 
     # ----------------------- КЛАВИАТУРА -----------------------
     def enter_key(self, locator):
+        """
+        Нажать Enter в элементе
+        :param locator: путь к элементу
+        :return: None
+        """
         element = self.find_element(locator)
         element.send_keys(Keys.ENTER)
 
